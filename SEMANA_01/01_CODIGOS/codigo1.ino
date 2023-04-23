2a0e5b669fe5317fe928fb665e773ff105520852
BrWasser <110609849+BrWasser@users.noreply.github.com>
2023-04-23 18:15:31 -0300

    Código inicial

---
+++ SEMANA_01/01_CODIGOS/codigo1.ino
@@ -0,0 +1,14 @@
+const float pi = 3.1415926;
+bool booleano = true; 
+int inteiro = 3;
+char string[]= {"codigo"};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(serial.available)
+}
